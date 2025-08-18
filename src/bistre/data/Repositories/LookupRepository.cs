ab26c8c376e383cfab294e35918551120e73f6f4
Ashleigh Clark <ashleigh.clark@kaleidocode.co.za>
2025-08-18 09:53:18 +0200

    MOD: Changed Derived Repository Overrides to call async methods appropriately.

--- src/bistre/data/Repositories/LookupRepository.cs
+++ src/bistre/data/Repositories/LookupRepository.cs
@@ -7,29 +7,29 @@ namespace Bistre.Data.Repositories;
 
 public class LookupRepository(DefaultContext context) : BaseRepository<LookupEntity>(context), IBaseRepository<LookupEntity>
 {
-    public sealed override Task InsertAsync(LookupEntity model, Guid? requesterId = null, Func<Exception, Task>? onFailureAsync = null)
+    public sealed async override Task InsertAsync(LookupEntity model, Guid? requesterId = null, Func<Exception, Task>? onFailureAsync = null)
     {
-        return base.InsertAsync(model, requesterId, onFailureAsync);
+        await base.InsertAsync(model, requesterId, onFailureAsync);
     }
 
-    public sealed override Task<LookupEntity?> GetByIdAsync<TId>(TId id, Func<Exception, Task>? onFailureAsync = null)
+    public sealed async override Task<LookupEntity?> GetByIdAsync<TId>(TId id, Func<Exception, Task>? onFailureAsync = null)
     {
-        return base.GetByIdAsync(id, onFailureAsync);
+        return await base.GetByIdAsync(id, onFailureAsync);
     }
 
-    public sealed override Task<IQueryable<LookupEntity>?> ListAsync(int limit, Func<Exception, Task>? onFailureAsync = null)
+    public sealed async override Task<IQueryable<LookupEntity>?> ListAsync(int limit, Func<Exception, Task>? onFailureAsync = null)
     {
-        return base.ListAsync(limit, onFailureAsync);
+        return await base.ListAsync(limit, onFailureAsync);
     }
 
-    public sealed override Task UpdateAsync(LookupEntity model, Guid? requesterId = null, Func<Exception, Task>? onFailureAsync = null)
+    public sealed async override Task UpdateAsync(LookupEntity model, Guid? requesterId = null, Func<Exception, Task>? onFailureAsync = null)
     {
-        return base.UpdateAsync(model, requesterId, onFailureAsync);
+        await base.UpdateAsync(model, requesterId, onFailureAsync);
     }
 
-    public sealed override Task DeleteAsync<TId>(TId id, Guid? requesterId = null, Func<Exception, Task>? onFailureAsync = null)
+    public sealed async override Task DeleteAsync<TId>(TId id, Guid? requesterId = null, Func<Exception, Task>? onFailureAsync = null)
     {
-        return base.DeleteAsync(id, requesterId, onFailureAsync);
+        await base.DeleteAsync(id, requesterId, onFailureAsync);
     }
 }
 
